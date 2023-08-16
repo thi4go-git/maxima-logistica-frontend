7f70fd83d03afb6930a194398d372b56236683f5
thi4go-git <thi4go19@gmail.com>
2023-08-16 19:41:19 -0300

    Teste unitário do endereço service

--- src/app/servicos/endereco.service.spec.ts
+++ src/app/servicos/endereco.service.spec.ts
@@ -1,16 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { EnderecoService } from './endereco.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { EnderecoDTOViacep } from '../entity/enderecoViaCepDTO';
+import { environment } from 'src/environments/environment';
 
-describe('EnderecoService', () => {
-  let service: EnderecoService;
+fdescribe('EnderecoService', () => {
+
+  let enderecoService: EnderecoService;
+  let httpMock: HttpTestingController; // Mock do HTTP para requisição mockada
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EnderecoService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    const testBed = getTestBed();
+    enderecoService = testBed.get(EnderecoService);
+    httpMock = testBed.get(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('Deve criar o componente EnderecoService', () => {
+    expect(enderecoService).toBeTruthy();
   });
+
+  it('Deve retornar endereco ViaCep', () => {
+
+    const cep = '74915660';
+
+    const mockRetornoCep: EnderecoDTOViacep = new EnderecoDTOViacep;
+    mockRetornoCep.bairro = 'bairro';
+    mockRetornoCep.cep = cep;
+    mockRetornoCep.complemento = 'complemento';
+    mockRetornoCep.ddd = 'ddd';
+
+    enderecoService.retornarEnderecoViaCep(cep).subscribe((retorno) => {
+      expect(retorno).toBe(mockRetornoCep);
+    });
+
+    const request = httpMock.expectOne(environment.apiUrl + '/api/enderecos/viacep/' + cep);
+    expect(request.request.method).toBe('GET');
+
+    httpMock.verify();
+
+  });
+
 });
