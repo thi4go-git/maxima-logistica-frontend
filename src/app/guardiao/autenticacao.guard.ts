4bbce53e73dab85e173804bbf807f974aa91b832
thi4go-git <thi4go19@gmail.com>
2023-08-16 14:54:08 -0300

    Implementando segurança com guards

---
+++ src/app/guardiao/autenticacao.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { KeycloakService } from '../servicos/keycloak.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Injectable({ providedIn: 'root' })
+export class AutenticacaoGuard implements CanActivate {
+
+  constructor(
+    private keycloakService: KeycloakService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    const autenticado = this.keycloakService.isAuthenticated();
+
+    if (autenticado) {
+      return true;
+    } else {
+      this.snackBar.open("Faça o Login para continuar!", "OBS!", {
+        duration: 2000
+      });
+      localStorage.clear();
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+
+}
