0d79adbb21514ec376e79dd8f10d7e02fe24388f
thi4go-git <thi4go19@gmail.com>
2023-08-17 03:12:11 -0300

    Teste inicial ClienteListaComponent

---
+++ src/app/modulos/cliente/cliente-lista/cliente-lista.component.spec.ts
@@ -0,0 +1,77 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ClienteListaComponent } from "./cliente-lista.component";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Router } from "@angular/router";
+import { AvisosDialogService } from "src/app/servicos/avisos-dialog.service";
+import { ClienteService } from "src/app/servicos/cliente.service";
+import { SharedModule } from "../../shared/shared.module";
+import { of, throwError } from "rxjs";
+import { ClienteDTOResourceList } from "src/app/entity/clienteDDTOResourceList";
+
+
+
+describe('ClienteListaComponent', () => {
+
+    let clienteListaComponent: ClienteListaComponent;
+    let fixture: ComponentFixture<ClienteListaComponent>;
+
+    let mockClienteService: any;
+    let mockMatSnackBar: any;
+    let mockAvisosDialogService: any;
+    let mockRouter: any;
+
+    beforeEach((() => {
+        mockClienteService = jasmine.createSpyObj('ClienteService', [
+            'listarTodosPaginadoFilter',
+            'deletarCliente'
+        ]);
+
+        mockMatSnackBar = jasmine.createSpyObj('MatSnackBar', [
+            'open'
+        ]);
+
+        mockAvisosDialogService = jasmine.createSpyObj('AvisosDialogService', [
+            'openConfirmationDialog'
+        ]);
+
+        mockRouter = jasmine.createSpyObj('Router', [
+            'navigate'
+        ]);
+
+        TestBed.configureTestingModule({
+            imports: [
+                SharedModule
+            ],
+            providers: [
+                { provide: ClienteService, useValue: mockClienteService },
+                { provide: MatSnackBar, useValue: mockMatSnackBar },
+                { provide: AvisosDialogService, useValue: mockAvisosDialogService },
+                { provide: Router, useValue: mockRouter }
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ClienteListaComponent);
+        clienteListaComponent = fixture.componentInstance;
+    });
+
+    it('Deve Criar o componente', () => {
+        expect(clienteListaComponent).toBeTruthy();
+    });
+
+    it('Teste onInit', () => {
+        spyOn(clienteListaComponent, 'listarTodosFiltro');
+        clienteListaComponent.ngOnInit();
+        expect(clienteListaComponent.listarTodosFiltro).toHaveBeenCalled();
+    });
+
+    fdescribe('Teste método listarTodosFiltro', () => {
+        it('Deve listarTodosPaginadoFilter ', () => {
+    
+        });
+
+    });
+
+});
