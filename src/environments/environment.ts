796cbb42aba932318375359f64cf085b7fe5c76a
thi4go-git <thi4go19@gmail.com>
2023-09-05 13:05:42 -0300

    Ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
     versao: 'v_16.08.2023',
     cliId: 'maxima-logistica',
     cliSecret: '17DmswLn5mwQGsmbRqjxLA3c8U0c4lN0',
-    tokenGoogleMaps: 'AIzaSyCQb7Mv4wxcRoqlZ9SS9Bxm5sc8dawI3Iw'
+    tokenGoogleMaps: 'meu token'
 };
